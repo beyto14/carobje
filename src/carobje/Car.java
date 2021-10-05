fea27d48431a7a5d907ffbd714de0548abd9cbdc
beyto14 <beytullahsaariz288@gmail.com>
2021-10-05 16:12:52 +0300

    Initial commit

---
+++ src/carobje/Car.java
@@ -0,0 +1,34 @@
+package carobje;
+
+public class Car {
+
+	//deðiþkenler
+	private String caption;
+	private int price;
+	
+	//set methodlarý
+	public void setcaption(String caption) {
+		this.caption=caption;
+	}
+	public void setprice(int price) {
+		this.price=price;
+	}	
+	
+	
+	
+	
+	//get methodlarý
+	public String getcaption() {
+		
+		
+		return this.getcaption();
+	}
+	   
+	
+		
+	}
+	
+	
+	
+	
+	
